18eb99f2bdd1561aca2018b23a3e4070876c06c8
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-15 19:07:42 +0530

    Calculating Daily Employee Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,11 +3,23 @@ class EmployeeWageBuilder
   public static void main(String[] args)
   {
 	int IS_FULL_TIME = 1;
+	int RATE_PER_HOUR = 20;
+	int empHrs = 0,empWage = 0;
+
 	System.out.println("----: Welcome to Employee Wage Builder :---");
+
 	double empCheck = Math.floor(Math.random() * 10 ) % 2;
+
 	if(empCheck == IS_FULL_TIME)
+	{
+		empHrs = 8;
 		System.out.println("Employee is Present");
+		empWage = empHrs * RATE_PER_HOUR;
+		System.out.println("Employee Daily Wage: " +empWage);
+	}
 	else
+	{
 		System.out.println("Employee is Absent");
-  }
+	}
+   }
 }
